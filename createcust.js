2bcb539429b699bf4f2299f4f715024a8f665066
akhil <akhil@akhils-MacBook-Air.local>
2021-01-20 00:13:13 +0530

    Debugging breakpoints added

--- createcust.js
+++ createcust.js
@@ -2,8 +2,10 @@ const express=require('express')
 const axios=require('axios')
 const config=require('./config')
 
+
 exports.postCreateCust=(req,res,next) => {
     const refnum=req.body.data.merchantRefNum
+    console.log('RefNum line 8 ',refnum)
 
     axios.get('https://api.test.paysafe.com/paymenthub/v1/customers?merchantCustomerId='+ refnum, {
         headers: {
@@ -12,8 +14,10 @@ exports.postCreateCust=(req,res,next) => {
         }
     })
     .then(res=>{
-        if(res.statsCode==201){
+        console.log('Result after fiding custid line 17 ',res)
+        if(res.statusCode==201){
             const custId=res.body.id
+            console.log('line 20 ',res.body)
             axios.post('https://api.test.paysafe.com/paymenthub/v1/customers/'+custId+'/singleusecustomertokens',{
                 "merchantRefNum": refnum,
                 "paymentTypes": [
@@ -25,6 +29,7 @@ exports.postCreateCust=(req,res,next) => {
                     'Content-Type':'application/json'
                 }
               }).then(res=>{
+                  console.log('after getting token line 32',res)
                   if(res.statusCode==200){
                     res.status(200).json({
                         message:'successful',
@@ -34,6 +39,56 @@ exports.postCreateCust=(req,res,next) => {
                   }
               }).catch(err => console.log(err))
         }
+        else{
+            console.log('not found line 43')
+            axios.post('https://api.test.paysafe.com/paymenthub/v1/customers',
+                {
+                    "merchantCustomerId": refnum,
+                    "locale": "en_US",
+                    "firstName": "John",
+                    "middleName": "James",
+                    "lastName": "Smith",
+                    "dateOfBirth": {
+                      "year": 1981,
+                      "month": 10,
+                      "day": 24
+                    },
+                    "email": "john.smith@email.com",
+                    "phone": "777-444-8888",
+                    "ip": "192.0.126.111",
+                    "gender": "M",
+                    "nationality": "Canadian",
+                    "cellPhone": "777-555-8888"
+                }
+                ,{
+                headers: {
+                    'Authorization':'Basic ' + config.key,
+                    'Content-Type':'application/json'
+                }}).then(res => {
+                    console.log('after creating custid line 68 ',res)
+                    const custId=res.body.id
+                    axios.post('https://api.test.paysafe.com/paymenthub/v1/customers/'+custId+'/singleusecustomertokens',{
+                            "merchantRefNum": refnum,
+                            "paymentTypes": [
+                                "CARD"
+                            ]               
+                            },{
+                            headers: {
+                                'Authorization':'Basic ' + config.key,
+                                'Content-Type':'application/json'
+                            }
+                            }).then(res=>{
+                                console.log('line 81',res)
+                                if(res.statusCode==200){
+                                    res.status(200).json({
+                                        message:'successful',
+                                        token:res.data.singleUseCustomerToken,
+                                        id:res.data.id
+                                        })
+                                }
+                            }).catch(err => console.log(err))
+                })
+            }
     })
     .catch(err => console.log(err))
 }
