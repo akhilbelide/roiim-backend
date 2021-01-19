74c368048ddcf9be1af38c80c2ffda2ba8f2d9ca
akhil <akhil@akhils-MacBook-Air.local>
2021-01-19 20:24:49 +0530

    Updated url

--- controllers/payapi.js
+++ controllers/payapi.js
@@ -8,7 +8,7 @@ exports.postPayment=(req,res,next)=>{
     console.log(req.body)
     var options = {
         'method': 'POST',
-        'url': 'https://private-anon-8467725ed3-paysafeapipaymenthubv1.apiary-proxy.com/paymenthub/v1/payments',
+        'url': 'https://api.test.paysafe.com/paymenthub/v1/payments',
         'headers': {
           'Content-Type': 'application/json',
           'Authorization': 'Basic cHJpdmF0ZS03NzUxOkItcWEyLTAtNWYwMzFjZGQtMC0zMDJkMDIxNDQ5NmJlODQ3MzJhMDFmNjkwMjY4ZDNiOGViNzJlNWI4Y2NmOTRlMjIwMjE1MDA4NTkxMzExN2YyZTFhODUzMTUwNWVlOGNjZmM4ZTk4ZGYzY2YxNzQ4'
@@ -21,7 +21,7 @@ exports.postPayment=(req,res,next)=>{
         console.log(response.body);
       });
 
-      res.status(200).json({data:{
+      res.status(200).json({
           message:'successful'
-      }})
+      })
 }
