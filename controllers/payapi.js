78ec29de61f90c61f2a5392feef296f965aab53e
akhil <akhil@akhils-MacBook-Air.local>
2021-01-19 20:36:20 +0530

    Removed MerchantCustId

--- controllers/payapi.js
+++ controllers/payapi.js
@@ -13,7 +13,7 @@ exports.postPayment=(req,res,next)=>{
           'Content-Type': 'application/json',
           'Authorization': 'Basic cHJpdmF0ZS03NzUxOkItcWEyLTAtNWYwMzFjZGQtMC0zMDJkMDIxNDQ5NmJlODQ3MzJhMDFmNjkwMjY4ZDNiOGViNzJlNWI4Y2NmOTRlMjIwMjE1MDA4NTkxMzExN2YyZTFhODUzMTUwNWVlOGNjZmM4ZTk4ZGYzY2YxNzQ4'
         },
-        body: JSON.stringify({"merchantRefNum":"akhil001","merchantCustomerId":req.body.custId,"amount":10000,"currencyCode":"USD","paymentHandleToken":req.body.paymentHandleToken,"description":"ROIIIM Akhil Assignment"})
+        body: JSON.stringify({"merchantRefNum":"akhil001","amount":10000,"currencyCode":"USD","paymentHandleToken":req.body.paymentHandleToken,"description":"ROIIIM Akhil Assignment"})
       
       };
       request(options, function (error, response) {
