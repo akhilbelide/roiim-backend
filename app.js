61617ef1d94ce5c9cf84cef17b15de9f2e1929c0
akhil <akhil@akhils-MacBook-Air.local>
2021-01-20 01:46:15 +0530

    Removed cors for ASH

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const payapiRoutes=require('./routes/payapi')
 const createcustController=require('./createcust')
 app.use(bodyparser.json())
 
-app.use(cors())
+//app.use(cors())
 
 app.use((req,res,next) => {
-    //res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
